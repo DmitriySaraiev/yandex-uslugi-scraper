636509467708c9374137346b393da03044c95a21
Dmitriy Saraiev <dimetriy1111@gmail.com>
2020-10-17 23:06:59 +0300

    Initial commit

---
+++ src/main/java/com/saraiev/yandexuslugiscraper/YandexUslugiScraperApplication.java
@@ -0,0 +1,13 @@
+package com.saraiev.yandexuslugiscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexUslugiScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexUslugiScraperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saraiev/yandexuslugiscraper/YandexUslugiScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraiev.yandexuslugiscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexUslugiScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
